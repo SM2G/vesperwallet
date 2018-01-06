77efe8d9f109b75384183389d19b4dd88c9bf69b
Samy Gejzenblozen <samy.gejz@gmail.com>
2018-01-06 18:02:32 +0100

    Use official Angular library
    Security improvment

--- README.md
+++ README.md
@@ -11,6 +11,26 @@ When you want to retrieve the funds, simply repeat the procedure to recover your
 
 Please note that this is a currently a *work in progress* project.
 
+# External components
+
+* [Standford Javascript Crypto Library](http://bitwiseshiftleft.github.io/sjcl/)
+
+* [Angular JS](https://angularjs.org/)
+
+* [Skeleton](http://getskeleton.com/)
+
+# Checksums
+
+If you prefer to use the Wallet offline, make sure your checksums match.
+
+```
+shasum -a 256 index.html
+129113fcd1f56f40ce46214727770b86f4129b2ba03e5d60f7956ec90efbf5df  index.html
+shasum -a 256 js/*
+d09a8688f37c7442bb1e6699b46efb191d9281ef05a492586fa0f54dc4e5110a  js/sjcl.js
+7cd6d05e48ae8923674c5304429873ae765cb0f884d7a71f6d49a4cda7068546  js/vesper.js
+```
+
 # Contribution policy
 
-All contributions welcome!
+All contributions welcome.
--- js/vesper.js
+++ js/vesper.js
@@ -8,6 +8,7 @@ app.controller('FormController', function () {
     this.reset = function() {
         this.passphrase = "";
         this.salt = "";
+        document.getElementById("seed").innerHTML = "<h5>Input your passphrase and e-mail as salt to generate a seed.</br><strong>Note:</strong> This seed is generated in your browser and not sent anywhere.</h5>";
     };
 
     this.genSeed = function(passphrase, salt) {
@@ -17,8 +18,8 @@ app.controller('FormController', function () {
         for(;seed.length < 81;seed += sjcl.codec.base64.fromBits(derivedKey).replace(/[^A-Z9]+/g, '')) {};
         seed = seed.substr(1, 81)
 
-        document.getElementById("seed").innerHTML = "<code>" + seed + "</code>"
-        + "</br>Key length: " + seed.length;
+        document.getElementById("seed").innerHTML = "<h5><code><strong>" + seed + "</strong></code>"
+        + "</br>Key length: <em>" + seed.length + "</em></h5>";
     };
 
 });

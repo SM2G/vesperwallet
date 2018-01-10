4805aed824691e616d87bb17399c1e8c1b85fd0c
Samy Gejzenblozen <samy.gejz@gmail.com>
2018-01-10 17:21:55 +0100

    Add splitted seed output

--- js/vesper.js
+++ js/vesper.js
@@ -8,7 +8,7 @@ app.controller('FormController', function () {
     this.reset = function() {
         this.passphrase = "";
         this.salt = "";
-        document.getElementById("seed").innerHTML = "<h5>Input your passphrase and e-mail as salt to generate a seed.</br><strong>Note:</strong> This seed is generated in your browser and not sent anywhere.</h5>";
+        document.getElementById("seed").innerHTML = "<h5><u>Instructions:</u></br>Input your passphrase and e-mail as salt to generate a seed.</br><strong>Note:</strong> This seed is generated in your browser and not sent anywhere.</h5>";
     };
 
     this.genSeed = function(passphrase, salt) {
@@ -18,8 +18,15 @@ app.controller('FormController', function () {
         for(;seed.length < 81;seed += sjcl.codec.base64.fromBits(derivedKey).replace(/[^A-Z9]+/g, '')) {};
         seed = seed.substr(1, 81)
 
+        splittedSeed = ""
+        split = seed.match(/.{1,5}/g)
+        for (i = 0; i < split.length; i++) {
+            splittedSeed += "<code>" + split[i] + "</code> ";
+        }
+
         document.getElementById("seed").innerHTML = "<h5><code><strong>" + seed + "</strong></code>"
-        + "</br>Key length: <em>" + seed.length + "</em></h5>";
+        + "</br><small><a>" + splittedSeed + "</a></small>"
+        + "</br><h5>Key length: <em>" + seed.length + "</em></h5>";
     };
 
 });
